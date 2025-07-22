a6a8f56c7d0b45c900bb74db49b9f4a296b2f439
AliRaza <wolfhuntin49.com>
2025-07-22 12:09:00 +0500

    Small fixs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 // import Hero3D from "@/components/Hero3D";
 import FeaturesGrid from "@/components/FeaturesGrid";
 import VideoSection from "@/components/VideoSection";
-import HeroSection from "@/components/HeroSection";
+// import HeroSection from "@/components/HeroSection";
+import Hero3D from "@/components/Hero3D";
 import MobileShowcase from "@/components/MobiuleShowcase";
 const page = () => {
   return (
     <>
-      <HeroSection/>
-      {/* <Hero3D /> */}
+      {/* <HeroSection/> */}
+      <Hero3D />
       <FeaturesGrid />
       <MobileShowcase/>
       <VideoSection />
